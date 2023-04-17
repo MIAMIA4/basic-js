78d732ea259af728cc79665bd52600b7c3b90ab0
Ilya <19962191a@gmail.com>
2023-04-18 01:35:25 +0300

    feat: add new sol

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const obj = {}
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse()
+    let current = ''
+    
+    for (const part of parts) {
+      current = `${current}.${part}`
+      
+      if (!obj[current]) {
+        obj[current] = 1
+      } else {
+        obj[current]++
+      }
+    }
+  }
+  
+  return obj
 }
 
 module.exports = {
